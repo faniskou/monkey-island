c05c1cdafcd2990c6d97d2a34d27636f4fcc84c3
fanis <faniskou@gmail.com>
2015-07-21 21:28:15 +0300

    new changes

--- PirateIsland/src/com/main/pirateisland/LoginScreen.java
+++ PirateIsland/src/com/main/pirateisland/LoginScreen.java
@@ -22,12 +22,13 @@ public class LoginScreen extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// check
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			return true;
+			return   true; 
 		}
 		return super.onOptionsItemSelected(item);
 	}
---
+++ PirateIsland/src/com/main/pirateisland/User.java
@@ -0,0 +1,19 @@
+package com.main.pirateisland;
+
+public class User {
+	
+	 //private variables
+    String _name;
+    int _years_old;
+    
+    // Empty constructor
+    public User(){
+         
+    }
+    public User(String name, int years_old){
+        this._name = name;
+        this._years_old = years_old;
+    }
+    
+
+}
