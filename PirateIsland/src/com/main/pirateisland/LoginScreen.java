cc3425a3a190b8960fcb91e7e22abd8a6eb944a3
fanis <faniskou@gmail.com>
2015-08-24 21:25:59 +0300

    sindesh me vash

--- PirateIsland/src/com/main/pirateisland/LoginScreen.java
+++ PirateIsland/src/com/main/pirateisland/LoginScreen.java
@@ -1,19 +1,97 @@
 package com.main.pirateisland;
 import java.sql.*;
-import javax.swing.*;
+//import javax.swing.*;
 
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginScreen extends ActionBarActivity {
 
+public class LoginScreen extends Activity {
+	
+	EditText editTextUserName,editTextPassword,editTextConfirmPassword;
+	Button btnCreateAccount,btnSignIn;
+	
+	logindatabaseadapter loginDataBaseAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_screen);
+		// get Instance  of Database Adapter
+		loginDataBaseAdapter=new logindatabaseadapter(this);
+		loginDataBaseAdapter=loginDataBaseAdapter.open();
+ 
+		// Get Refferences of Views
+		editTextUserName=(EditText)findViewById(R.id.editTextUserNameToLogin);
+		editTextPassword=(EditText)findViewById(R.id.editTextPasswordToLogin);
+		//fanis to be deleted
+		editTextConfirmPassword=(EditText)findViewById(R.id.editTextPasswordToLogin);
+ 
+		btnSignIn=(Button)findViewById(R.id.buttonSignIn);
+		btnCreateAccount=(Button)findViewById(R.id.buttonCreateAccount);
+		btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+ 
+		public void onClick(View v) {
+
+			
+			// TODO Auto-generated method stub
+ 
+			String userName=editTextUserName.getText().toString();
+			String password=editTextPassword.getText().toString();
+			String confirmPassword=editTextConfirmPassword.getText().toString();
+ 
+			// check if any of the fields are vaccant
+			if(userName.equals("")||password.equals("")||confirmPassword.equals(""))
+			{
+					Toast.makeText(getApplicationContext(), "Field Vaccant", Toast.LENGTH_LONG).show();
+					return;
+			}
+			// check if both password matches
+			if(!password.equals(confirmPassword))
+			{
+				Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
+				return;
+			}
+			else
+			{
+			    // Save the Data in Database
+			    loginDataBaseAdapter.insertEntry(userName, password);
+			    Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
+			}
+			
+		}
+		
+		
+		});
+		// Set On ClickListener
+		btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// get The User name and Password
+				String userName=editTextUserName.getText().toString();
+				String password=editTextPassword.getText().toString();
+
+				// fetch the Password form database for respective user name
+				String storedPassword=loginDataBaseAdapter.getSinlgeEntry(userName);
+
+				// check if the Stored password matches with  Password entered by user
+				if(password.equals(storedPassword))
+				{
+					Toast.makeText(getApplicationContext(), "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+					//fanis we must from here go to main application 
+				}
+				else
+				{
+					Toast.makeText(getApplicationContext(), "User Name or Password does not match", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
 
 	@Override
