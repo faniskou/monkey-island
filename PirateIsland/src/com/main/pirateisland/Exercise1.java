c8e3729e51d8c57339e54da123a8bd39a279577b
G90DD <georgek1990bb@gmail.com>
2015-09-11 22:45:46 +0300

    hints stin prosthesi
    evala mia metavliti (errorflag) i opoia anevainei se kathe lanthasmeni
    prospatheia
    -sto 1o lathos apla leei an einai megalutero i mikrotero tou noumero
    -sto 2o lathos sou vgazei 4 dunates luseis
    -sto 3o lathos tha xromatizei tin sosti apadisi kokini (tha to fiakso to
    vradaki auto)
    
    Menoun kapoia kena alla trexei kanonika edo.

--- PirateIsland/src/com/main/pirateisland/Exercise1.java
+++ PirateIsland/src/com/main/pirateisland/Exercise1.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import android.widget.EditText;
 
 public class Exercise1 extends Activity {
+	
+	int errorflag = 0; 
+	int A;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,12 @@ public class Exercise1 extends Activity {
 		
 		final int  random1 = (int) ((Math.random() * 10 ) + 1);
 		final int  random2 = (int) ((Math.random() * 10 ) + 1);
+		
+		final int  result = random1 +random2;
+		
+		 final int A = result - 3;
+         final int B = result - 1;
+    	 final int C = result + 1;
 		
 		 Button button1 = (Button) findViewById(R.id.button1);
 	     button1.setOnClickListener(new OnClickListener() {
@@ -52,27 +61,95 @@ public class Exercise1 extends Activity {
 	    		
 	    		EditText editText1 = (EditText) findViewById(R.id.editText1);
 	    		
-	    		int  result = random1 +random2;
+	    	   //int  result = random1 +random2;
 	    		
 	    		int answer = Integer.parseInt(editText1.getText().toString());
 	     
-	    		if (answer == result){
+	    		//if (answer == result){
 			    	 
-			    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 textView6.setText("οΏ½οΏ½οΏ½οΏ½οΏ½!");
-			     }
+			    	// TextView textView6 = (TextView) findViewById(R.id.textView6);
+			    	 //textView6.setText("Σωστά!");
+			     //}
+	    		
+	    		if (errorflag == 2){
+	    			
+	    			if (answer == result){
+				    	 
+				    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
+				    	 textView6.setText("Σωστά!!!!");	
+				    	 
+	    			}
+	    			
+	    			
+	    		}
+	    		
+	    		
+	    		if (errorflag == 1) {
+	    			
+	    			if (answer == result){
+				    	 
+				    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
+				    	 textView6.setText("Σωστά!!!!");
+				    	 }
+	    			
+	    			else {
+	    			 TextView textView6 = (TextView) findViewById(R.id.textView6);
+			    	 textView6.setText("Για δοκίμασε ξανά. Αυτή τη φορά επέλεξε έναν από τους 4 αριθμούς");
+		           
+			    	 
+			    	 //int A = result - 3;
+			         //int B = result - 1;
+			    	 //int C = result + 1;
+			    
+			    	 TextView textView7 = (TextView) findViewById(R.id.textView7);
+			    	 textView7.setText(String.valueOf(A));
+			    	 
+			    	 TextView textView8 = (TextView) findViewById(R.id.textView8);
+			    	 textView8.setText(String.valueOf(B));
+			    	 
+			    	 TextView textView9 = (TextView) findViewById(R.id.textView9);
+			         textView9.setText(String.valueOf(result));
+			    	 
+			    	 TextView textView10 = (TextView) findViewById(R.id.textView10);
+			    	 textView10.setText(String.valueOf(C));
+			    	 
+			    	 errorflag = 2;
+	    			}
+			    	 
+                    
+	    		
+			    	 
+		    		}
+	    		
+	    		
+	    		
+	    		
+	    		if (errorflag == 0) {
+	    			
+	    			
+	    		if (answer == result){
+				    	 
+				    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
+				    	 textView6.setText("Σωστά!");
+	    		}
 			     
 	             if (answer > result){
 			    	 
 			    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 textView6.setText("οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½, οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½! οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½.");
+			    	 textView6.setText("Λυπάμε, έκανες λάθος. Δοκίμασε ένα ποιό μικρό νούμερο.");
+			    	 errorflag = 1;
 			     }
 	             
 	             if (answer < result){
 			    	 
 			    	 TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 textView6.setText("οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½, οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½! οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½.");
+			    	 textView6.setText("Λυπάμε, έκανες λάθος. Δοκίμασε ένα ποιό μεγάλο νούμερο.");
+			    	 errorflag = 1;
+			    	 
 			     }
+	    		}
+	    		
+	    		
 				
 			}
 		}); 
