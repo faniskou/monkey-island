4a74f2e7735c0aa01546b5f479b0d779d11805eb
fanis <faniskou@gmail.com>
2015-09-16 20:04:55 +0300

    add a new activity for creation of the main map

---
+++ PirateIsland/src/com/main/pirateisland/MainActivity.java
@@ -0,0 +1,45 @@
+package com.main.pirateisland;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import android.widget.TextView;
+
+public class MainActivity extends Activity{
+	//protected String name;
+	//protected String pass;
+	TextView textname,textpass;
+	Button button1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+
+		textname=(TextView)findViewById(R.id.text_name);
+		textpass=(TextView)findViewById(R.id.text_pass);
+
+		Intent i= getIntent();
+		textname.setText(i.getStringExtra("name"));
+	    textpass.setText(i.getStringExtra("pass"));
+	    
+	    
+	    button1 = (Button) findViewById(R.id.button1);
+		 
+		// Capture button clicks
+		button1.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+ 
+				// Start NewActivity.class
+				Intent myIntent = new Intent(MainActivity.this,
+						Exercise1.class);
+				startActivity(myIntent);
+			}
+		});
+	}
+
+}
