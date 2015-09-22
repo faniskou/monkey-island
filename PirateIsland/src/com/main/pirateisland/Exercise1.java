d9f4445b0e7d2a95e2271d2b2242503fe0ec4355
G90DD <georgek1990bb@gmail.com>
2015-09-23 02:30:44 +0300

    ex1- functions
    ksanagrapsa ligo ton kodika tis 1 na fainete kalutera

--- PirateIsland/src/com/main/pirateisland/Exercise1.java
+++ PirateIsland/src/com/main/pirateisland/Exercise1.java
@@ -16,52 +16,31 @@ import android.widget.Toast;
 public class Exercise1 extends Activity {
 	
 	int errorflag = 0; 
-	int A;
 	int MyException;
-	//String aaa;
+	final int  random1 = (int) ((Math.random() * 10 ) + 1);
+	final int  random2 = (int) ((Math.random() * 10 ) + 1);
+	final int  result = random1 +random2;
+	final int A = result - 3;
+    final int B = result - 1;
+	final int C = result + 1;
+	
 	logindatabaseadapter loginDataBaseAdapter;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exercise1);
 		
-		
-		//monimes metavlites, den allazoun
-		//gia allagi me to patioma enos koumpiou, orizoume mesa sta koumpia k oxi final
-		
-		
-		//aaa =(TextView)findViewById(R.id.textview11);
-		//Intent i= getIntent();
-		//aaa.setText(i.getStringExtra("name"));
-	    
 		//to Onoma tou xristi einai stin "name"
-		
-		final int  random1 = (int) ((Math.random() * 10 ) + 1);
-		final int  random2 = (int) ((Math.random() * 10 ) + 1);
-		
-		final int  result = random1 +random2;
-		
-		 final int A = result - 3;
-         final int B = result - 1;
-    	 final int C = result + 1;
-		
-    	 TextView textView1 = (TextView) findViewById(R.id.textView1);
+	
+         TextView textView1 = (TextView) findViewById(R.id.textView1);
 	     textView1.setText(String.valueOf(random1)); 
 	     
 	     TextView textView3 = (TextView) findViewById(R.id.textView3);
 	     textView3.setText(String.valueOf(random2));
-	     
-	     TextView textView4 = (TextView) findViewById(R.id.textView4);
-	     textView4.setText("+");
-	     
-	     TextView textView5 = (TextView) findViewById(R.id.textView5);
-	     textView5.setText("=");
     	 
-	     //loginDataBaseAdapter=new logindatabaseadapter(this);
-			//loginDataBaseAdapter=loginDataBaseAdapter.open();
-    	 
-		
-	     
+	   
 	     Button button2 = (Button) findViewById(R.id.button2);
 	     button2.setOnClickListener(new OnClickListener() {
 			
@@ -71,149 +50,109 @@ public class Exercise1 extends Activity {
 	    		
 	    		EditText editText1 = (EditText) findViewById(R.id.editText1);
 	    		
-	    	   //int  result = random1 +random2;
-	    		
 	    		int answer = Integer.parseInt(editText1.getText().toString());
 	    		
 	    		if (errorflag == 3){
-	    			
-	    			if (answer == result){
-				    	 
-				    	// TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	 //textView6.setText("Σωστά!!!!");	
-				    	 // loginDataBaseAdapter.updateEX(errorflag);
-						// Toast.makeText(getApplicationContext(), "Success ", Toast.LENGTH_LONG).show();
-	    				Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
-		    			toast.show();
-				    	 
-				    	 
-	    			}
-	    			else {
-	                    // TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	 //textView6.setText("Για σκέψου το καλύτερα!");
-	    				Toast toast = Toast.makeText(getApplicationContext(), "Για σκέψου το καλύτερα!", Toast.LENGTH_SHORT);
-		    			toast.show();
-	    			}
-	    			
-	    			
+	    			check3(answer);
 	    		}
-	    		
 	    		if (errorflag == 2){
-	    			
-	    			if (answer == result){
-	    				//TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	 //textView6.setText("Σωστά!!!!");	
-	    				Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
-		    			toast.show();
-	    			}
-	    			else {
-	    				TextView textView7 = (TextView) findViewById(R.id.textView7);
-	    				textView7.setTextColor(Color.parseColor("#FFFFFF"));
-	    				
-	    				TextView textView8 = (TextView) findViewById(R.id.textView8);
-	    				textView8.setTextColor(Color.parseColor("#FF0000"));
-	    				
-	    				TextView textView9 = (TextView) findViewById(R.id.textView9);
-	    				textView9.setTextColor(Color.parseColor("#FFFFFF"));
-	    				
-	    				TextView textView10 = (TextView) findViewById(R.id.textView10);
-	    				textView10.setTextColor(Color.parseColor("#FFFFFF"));
-	    				
-	    				//TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	// textView6.setText("Για σκέψου το καλύτερα!");
-	    				Toast toast = Toast.makeText(getApplicationContext(), "Για σκέψου το καλύτερα!", Toast.LENGTH_SHORT);
-		    			toast.show();
-				    	 errorflag = 3;
-	    			}
-	    			
-	    			
+	    			check2(answer);
 	    		}
-	    		
-	    		
 	    		if (errorflag == 1) {
-	    			
-	    			if (answer == result){
-				    	 //TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	 //textView6.setText("Σωστά!!!!");
-	    				 Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
-			    			toast.show();
-				    	 }
-	    			
-	    			else {
-	    				 Toast toast = Toast.makeText(getApplicationContext(), "Λάθος. Επέλεξε έναν από τους 4 αριθμούς", Toast.LENGTH_SHORT);
-			    			toast.show();
-	    				
-	    			// TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 //textView6.setText("Λάθος. Επέλεξε έναν από τους 4 αριθμούς");
-		           //int A = result - 3;
-			         //int B = result - 1;
-			    	 //int C = result + 1;
-			    
-			    	 TextView textView7 = (TextView) findViewById(R.id.textView7);
-			    	 textView7.setText(String.valueOf(A));
-			    	 
-			    	 TextView textView8 = (TextView) findViewById(R.id.textView8);
-			    	 textView8.setText(String.valueOf(result));
-			    	 
-			    	 TextView textView9 = (TextView) findViewById(R.id.textView9);
-			         textView9.setText(String.valueOf(B));
-			    	 
-			    	 TextView textView10 = (TextView) findViewById(R.id.textView10);
-			    	 textView10.setText(String.valueOf(C));
-			    	 
-			    	 errorflag = 2;
-	    			}
-			    	 
-                    
-	    		
-			    	 
-		    		}
-	    		
-	    		
-	    		
-	    		
+	    			check1(answer);
+			    }
 	    		if (errorflag == 0) {
-	    			
-	    			
-	    		if (answer == result){
-	    			
-	    		    Toast toast = Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT);
-	    			toast.show();
-
-				    	 //TextView textView6 = (TextView) findViewById(R.id.textView6);
-				    	 //textView6.setText("Σωστά!");
-	    		}
-			     
-	             if (answer > result){
-			    	 
-			    	 //TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 //textView6.setText("Λαθος, δοκίμασε ένα ποιό μικρό νούμερο.");
-	            	 Toast toast = Toast.makeText(getApplicationContext(), "Λαθος, δοκίμασε ένα ποιό μικρό νούμερο.", Toast.LENGTH_SHORT);
-		    			toast.show();
-			    	 errorflag = 1;
-			     }
-	             
-	             if (answer < result){
-			    	 
-			    	 //TextView textView6 = (TextView) findViewById(R.id.textView6);
-			    	 //textView6.setText("Λάθος, δοκίμασε ένα ποιό μεγάλο νούμερο.");
-	            	 Toast toast = Toast.makeText(getApplicationContext(), "Λαθος, δοκίμασε ένα ποιό μεγάλο νούμερο.", Toast.LENGTH_SHORT);
-		    			toast.show();
-	            	 errorflag = 1;
-			    	 
-			     }
-	    		}
-	    		
-	    		
-				
-			}
-		}); 
-		
-		
-		
-		
+	    			check0(answer);	    		}
+			    }
+		}); 	
 	}
+	
+	
+	public void check0(int answer) {
+		
+		if (answer == result){
+			Toast toast = Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+		if (answer > result){
+			Toast toast = Toast.makeText(getApplicationContext(), "Λαθος, δοκίμασε ένα ποιό μικρό νούμερο.", Toast.LENGTH_SHORT);
+    			toast.show();
+	    	 errorflag = 1;
+	     }
+		if (answer < result){
+			Toast toast = Toast.makeText(getApplicationContext(), "Λαθος, δοκίμασε ένα ποιό μεγάλο νούμερο.", Toast.LENGTH_SHORT);
+    			toast.show();
+        	 errorflag = 1;
+	    }
+     }
 
+	public void check1 (int answer){
+		if (answer == result){
+			 Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
+   			toast.show();
+	    	 }
+		
+		else {
+			 Toast toast = Toast.makeText(getApplicationContext(), "Λάθος. Επέλεξε έναν από τους 4 αριθμούς", Toast.LENGTH_SHORT);
+   			toast.show();
+   
+   	        TextView textView7 = (TextView) findViewById(R.id.textView7);
+   	        textView7.setText(String.valueOf(A));
+   	  
+   	        TextView textView8 = (TextView) findViewById(R.id.textView8);
+        	 textView8.setText(String.valueOf(result));
+   	 
+   	         TextView textView9 = (TextView) findViewById(R.id.textView9);
+              textView9.setText(String.valueOf(B));
+   	 
+   	         TextView textView10 = (TextView) findViewById(R.id.textView10);
+   	          textView10.setText(String.valueOf(C));
+   	 
+   	 errorflag = 2;
+		}
+	}
+	
+	public void check2(int answer){
+		
+		if (answer == result){
+			
+			Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+		else {
+			TextView textView7 = (TextView) findViewById(R.id.textView7);
+			textView7.setTextColor(Color.parseColor("#FFFFFF"));
+			
+			TextView textView8 = (TextView) findViewById(R.id.textView8);
+			textView8.setTextColor(Color.parseColor("#FF0000"));
+			
+			TextView textView9 = (TextView) findViewById(R.id.textView9);
+			textView9.setTextColor(Color.parseColor("#FFFFFF"));
+			
+			TextView textView10 = (TextView) findViewById(R.id.textView10);
+			textView10.setTextColor(Color.parseColor("#FFFFFF"));
+			
+			
+			Toast toast = Toast.makeText(getApplicationContext(), "Για σκέψου το καλύτερα!", Toast.LENGTH_SHORT);
+			toast.show();
+	    	 errorflag = 3;
+		}		
+	}
+	
+	public void check3(int answer){
+		if (answer == result){
+	    	
+			Toast toast = Toast.makeText(getApplicationContext(), "Σωστά!", Toast.LENGTH_SHORT);
+			toast.show(); 
+		}
+		else {
+            
+			Toast toast = Toast.makeText(getApplicationContext(), "Για σκέψου το καλύτερα!", Toast.LENGTH_SHORT);
+			toast.show();
+		}	
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
